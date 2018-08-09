3d80b346a20ab4ab13796be795aab71a366dceaa
crislawrence <criswlawrence@gmail.com>
2018-08-09 15:02:32 -0400

    Some clean up.  Also added uniform breakage distribution and added python file to perform dash/plotly graphics.

--- lib/beers/PolyAStep.py
+++ lib/beers/PolyAStep.py
@@ -5,8 +5,8 @@ import sys
 
 class PolyAStep:
 
-    def __init__(self, log_file, parameters):
-        self.history_filename = log_file
+    def __init__(self, log_filename, parameters):
+        self.log_filename = log_filename
         self.min_retention_prob = parameters.get("min_retention_prob")
         self.max_retention_prob = parameters.get("max_retention_prob")
         self.length_retention_prob = parameters.get("length_retention_prob")
@@ -16,12 +16,20 @@ class PolyAStep:
         print("Poly A selection step instantiated")
 
     def execute(self, sample):
+        """
+        Remove nearly all molecules that don't have a poly A tail.  Most molecules with a poly A tail are retained.
+        Some bleed over modeled to occur in both directions.  Additionally, retained molecules have a chance of being
+        truncated on the 5' end.
+        :param sample: rna molecules subject to the poly A selection step
+        :return: rna molecules, possibly modified, that remain following the poly A selection step.
+        """
         print("Poly A selection step starting")
         retained_sample = []
-        with open(self.history_filename, "w+") as history_file:
-            history_file.write(Molecule.header)
-            print(f"sample size in {len(sample)}")
+        with open(self.log_filename, "w+") as log_file:
+            log_file.write(Molecule.header)
             for molecule in sample:
+
+                # Weighted distribution based on tail length
                 tail_length = molecule.poly_a_tail_length()
                 retention_odds = min(self.min_retention_prob + self.length_retention_prob * tail_length, self.max_retention_prob)
                 retained = np.random.choice([1, 0], 1, p=[retention_odds, 1 - retention_odds])[0]
@@ -29,20 +37,25 @@ class PolyAStep:
                 if retained:
                     retained_sample.append(molecule)
                     note += 'retained'
-                    note = self.three_prime_bias(molecule, tail_length, note)
+                    note = self.apply_three_prime_bias(molecule, tail_length, note)
                 else:
                     note += 'removed'
-                #print(tail_length, retention_odds, note)
-                history_file.write(molecule.log_entry(note))
+                log_file.write(molecule.log_entry(note))
         print("Poly A selection step complete")
         return retained_sample
 
-    def three_prime_bias(self, molecule, tail_length, note):
+    def apply_three_prime_bias(self, molecule, tail_length, note):
         breakage_likelihood = min([self.min_breakage_prob * (len(molecule.sequence) - tail_length), self.max_breakage_prob])
         breakage = np.random.choice([1, 0], 1, p=[breakage_likelihood, 1 - breakage_likelihood])
         if breakage:
-            breakpoint_from_3p = min(np.random.geometric(self.breakpoint_prob),len(molecule.sequence) - tail_length)
-            breakpoint_from_5p = len(molecule.sequence) - 1 - tail_length - breakpoint_from_3p
+
+            # Geometric distribution of breakpoints
+            # breakpoint_from_3p = min(np.random.geometric(self.breakpoint_prob), len(molecule.sequence) - tail_length)
+            # breakpoint_from_5p = len(molecule.sequence) - 1 - tail_length - breakpoint_from_3p
+
+            # Linear distribution of breakpoints
+            breakpoint_from_5p = np.random.randint(0, len(molecule.sequence) - tail_length)
+
             molecule.truncate(breakpoint_from_5p)
             note += ' broken'
         return note
@@ -70,20 +83,22 @@ if __name__ == "__main__":
         for text in sequences:
             sequence = text.rstrip()
             cigar = str(len(sequence)) + 'M'
-            molecule = Molecule(i, sequence, 1, cigar)
-            molecules.append(molecule)
+            rna_molecule = Molecule(i, sequence, 1, cigar)
+            molecules.append(rna_molecule)
             i += 1
-    log_file =  "../../data/polyA_selection_step_log.txt"
-    parameters = {
-        "min_retention_prob": 0.02,
-        "max_retention_prob": 0.98,
+    input_data_log_file = "../../data/polyA_step_input_data.log"
+    with open(input_data_log_file, "w+") as input_data_log:
+        input_data_log.write(Molecule.header)
+        for rna_molecule in molecules:
+            input_data_log.write(rna_molecule.log_entry())
+    output_data_log_file = "../../data/polyA_step_output_data.log"
+    input_parameters = {
+        "min_retention_prob": 0.01,
+        "max_retention_prob": 0.99,
         "length_retention_prob": 0.04,
-        "min_breakage_prob": 0.0005,
+        "min_breakage_prob": 0.005,
         "max_breakage_prob": 0.98,
-        "breakpoint_prob": 0.001
+        "breakpoint_prob": 0.0001
       }
-    step = PolyAStep(log_file, parameters)
-    new_sample = step.execute(molecules)
-    with open("../../data/polyASelectionStepTest.txt", "w+") as results:
-        for molecule in new_sample:
-            results.write(molecule.sequence)
+    step = PolyAStep(output_data_log_file, input_parameters)
+    step.execute(molecules)
