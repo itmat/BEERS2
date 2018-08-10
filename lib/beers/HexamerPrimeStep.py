beba8536c8126ff56ca76a6542e65c159132f697
brainfood <967448+brainfood@users.noreply.github.com>
2018-08-10 17:15:44 -0400

    Implement naive hexamer binding where all fragments primed at their 3' ends.

--- lib/beers/HexamerPrimeStep.py
+++ lib/beers/HexamerPrimeStep.py
@@ -1,12 +1,26 @@
+from molecule import Molecule
+
 class HexamerPrimeStep:
 
     def __init__(self, log_file, parameters):
+        self.history_filename = log_file
         print("Hexamer_primer_step instantiated")
 
     def execute(self, sample):
-        print("Hexamer prime step acting on sample")
-        return f"Post Hexamer prime step sample"
+        """
+        Starting with a basic implementation that assumes all fragments get
+        primed perfectly at their 3' ends.
+        """
+        print("Hexamer prime step starting")
+        primed_sample = []
+        primed_sample = sample
+        with open(self.history_filename, "w+") as log_file:
+            log_file.write(Molecule.header)
+            for molecule in primed_sample:
+                log_file.write(molecule.log_entry())
+        print("Hexamer prime step complete")
+        return primed_sample
 
     def validate(self):
-        print(f"Hexamer prime step validating parameters")
+        print("Hexamer prime step validating parameters")
         return True
