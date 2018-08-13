2f1b57ddc7a4d40cda06d106d80382389953e7e4
brainfood <967448+brainfood@users.noreply.github.com>
2018-08-13 14:08:53 -0400

    Update Molecule class to track and return list of bound molecules. Update HexamerPrimeStep class to represent hexamers as bound molecules for each molecule in the input sample.

--- lib/beers/HexamerPrimeStep.py
+++ lib/beers/HexamerPrimeStep.py
@@ -1,4 +1,5 @@
 from molecule import Molecule
+from utils import Utils
 
 class HexamerPrimeStep:
 
@@ -12,12 +13,18 @@ class HexamerPrimeStep:
         primed perfectly at their 3' ends.
         """
         print("Hexamer prime step starting")
+        primer_length = 6
         primed_sample = []
-        primed_sample = sample
         with open(self.history_filename, "w+") as log_file:
             log_file.write(Molecule.header)
-            for molecule in primed_sample:
-                log_file.write(molecule.log_entry())
+            for molecule in sample:
+                primer_sequence = Utils.create_complement_strand(molecule.sequence[-primer_length:])
+                primer_start = len(molecule) - primer_length
+                primer_num = 1
+                primer_molecule = Molecule(primer_num, primer_sequence, primer_start)
+                molecule.bind(primer_molecule)
+                primed_sample.append(molecule)
+                log_file.write(molecule.log_entry("Primers bound - " + molecule.print_bound_molecules()))
         print("Hexamer prime step complete")
         return primed_sample
 
