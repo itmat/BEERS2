ab1fe2e028b1ce3198a1981cf7a7b89368301c97
brainfood <967448+brainfood@users.noreply.github.com>
2018-10-30 16:24:48 -0400

    Rename first strand prime and cDNA synthesis modules so they are more general and follow pythonic conventions. Create analagous modules for second strand priming and synthesis modules.

--- beers/library_prep/first_strand_synthesis_step.py
+++ beers/library_prep/first_strand_synthesis_step.py
@@ -1,5 +1,5 @@
-from .molecule import Molecule
-from ..utilities.utils import Utils
+from beers.library_prep.molecule import Molecule
+from beers.utilities.utils import Utils
 
 
 class FirstStrandSynthesisStep:
