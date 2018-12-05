a7c2be3556c9c943b932f0d0153c938ef1fc2331
Cris Lawrence <cris.lawrence@verizon.net>
2018-12-05 14:49:39 -0500

    Modified bsub command per Tom and Dimitra's suggestions

--- beers/dispatcher.py
+++ beers/dispatcher.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import re
 import json
 from beers.library_prep.library_prep_pipeline import LibraryPrepPipeline
 from multiprocessing import Pool
@@ -7,15 +8,16 @@ from multiprocessing import Pool
 class Dispatcher:
 
     next_molecule_packet_id = 1
+    pkt_number_pattern = '^.*pkt(/d+).gzip$'
+
 
     def __init__(self, stage_name, configuration, input_directory_path, output_directory_path):
         self.stage_name = stage_name
         self.configuration = configuration
         self.input_directory_path = input_directory_path
         self.output_directory_path = output_directory_path
-        log_directory_path = os.path.join(output_directory_path, "logs")
-        self.std_ouput_file_path = os.path.join(log_directory_path, 'std_out.txt')
-        self.std_error_file_path = os.path.join(log_directory_path, 'std_err.txt')
+        self.log_directory_path = os.path.join(output_directory_path, "logs")
+        leading_roman_pattern = '^([ivx]+)(.*)$'
 
     def dispatch(self, method, molecule_packet_filenames):
         if method == 'multicore':
@@ -41,17 +43,21 @@ class Dispatcher:
         # TODO - class cannot be hardcoded
         pool.starmap(LibraryPrepPipeline.main, data)
 
-    def dispatch_pmacs(self, molecule_packet_filenames):
+    def dispatch_pmacs(self, packet_filenames):
         stage_configuration = json.dumps(self.configuration[self.stage_name])
         stage_process = f"./run_{self.stage_name}.py"
-        for molecule_packet_filename in molecule_packet_filenames:
+        for packet_filename in packet_filenames:
+            pkt_number_match = re.match(Dispatcher.pkt_number_pattern, packet_filename)
+            packet_id = pkt_number_match.group(1)
             # result = subprocess.call(
             #     f"bsub -o {self.std_ouput_file_path} -e {self.std_error_file_path} -J "
             #     f"python ../beers/library_prep/library_prep_pipeline.py"
             #     f" -c '{stage_configuration}' -o {self.output_directory_path}"
             #     f" -p {molecule_packet_filename}", shell=True)
+            std_ouput_file_path = os.path.join(self.log_directory_path, f'std_out_{packet_id}.txt')
+            std_error_file_path = os.path.join(self.log_directory_path, f'std_err_{packet_id}.txt')
             result = subprocess.call(
-                f"bsub -o {self.std_ouput_file_path} -e {self.std_error_file_path} -J "
+                f"bsub -o {std_ouput_file_path} -e {std_error_file_path} -J {self.stage_name}_{packet_id} "
                 f"{stage_process}"
                 f" -c '{stage_configuration}' -i {self.input_directory_path} -o {self.output_directory_path}"
-                f" -p {molecule_packet_filename}", shell=True)
+                f" -p {packet_filename}", shell=True)
