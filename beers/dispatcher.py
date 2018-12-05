f71bff6eb7ad704ff88d1cc17d6e1456951c016a
Cris Lawrence <cris.lawrence@verizon.net>
2018-12-05 15:08:42 -0500

    Trying to get bsub to work still

--- beers/dispatcher.py
+++ beers/dispatcher.py
@@ -8,8 +8,6 @@ from multiprocessing import Pool
 class Dispatcher:
 
     next_molecule_packet_id = 1
-    pkt_number_pattern = '^.*pkt(/d+).gzip$'
-
 
     def __init__(self, stage_name, configuration, input_directory_path, output_directory_path):
         self.stage_name = stage_name
@@ -27,14 +25,14 @@ class Dispatcher:
         else:
             self.dispatch_serial(molecule_packet_filenames)
 
-    def dispatch_serial(self, molecule_packet_filenames):
+    def dispatch_serial(self, packet_filenames):
         stage_configuration = json.dumps(self.configuration[self.stage_name])
         stage_process = f"./run_{self.stage_name}.py"
-        for molecule_packet_filename in molecule_packet_filenames:
+        for packet_filename in packet_filenames:
             result = subprocess.call(
             f"{stage_process}"
             f" -c '{stage_configuration}' -i {self.input_directory_path} -o {self.output_directory_path}"
-            f" -p {molecule_packet_filename}", shell=True)
+            f" -p {packet_filename}", shell=True)
 
     def dispatch_multicore(self, molecule_packet_filenames):
         stage_configuration = json.dumps(self.configuration[self.stage_name])
@@ -46,18 +44,16 @@ class Dispatcher:
     def dispatch_pmacs(self, packet_filenames):
         stage_configuration = json.dumps(self.configuration[self.stage_name])
         stage_process = f"./run_{self.stage_name}.py"
-        for packet_filename in packet_filenames:
-            pkt_number_match = re.match(Dispatcher.pkt_number_pattern, packet_filename)
-            packet_id = pkt_number_match.group(1)
+        for ctr, packet_filename in enumerate(packet_filenames):
             # result = subprocess.call(
             #     f"bsub -o {self.std_ouput_file_path} -e {self.std_error_file_path} -J "
             #     f"python ../beers/library_prep/library_prep_pipeline.py"
             #     f" -c '{stage_configuration}' -o {self.output_directory_path}"
             #     f" -p {molecule_packet_filename}", shell=True)
-            std_ouput_file_path = os.path.join(self.log_directory_path, f'std_out_{packet_id}.txt')
-            std_error_file_path = os.path.join(self.log_directory_path, f'std_err_{packet_id}.txt')
+            std_ouput_file_path = os.path.join(self.log_directory_path, f'std_out_{ctr}.txt')
+            std_error_file_path = os.path.join(self.log_directory_path, f'std_err_{ctr}.txt')
             result = subprocess.call(
-                f"bsub -o {std_ouput_file_path} -e {std_error_file_path} -J {self.stage_name}_{packet_id} "
+                f"bsub -o {std_ouput_file_path} -e {std_error_file_path} -J {self.stage_name}_{ctr} "
                 f"{stage_process}"
                 f" -c '{stage_configuration}' -i {self.input_directory_path} -o {self.output_directory_path}"
                 f" -p {packet_filename}", shell=True)
