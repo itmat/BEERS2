53fff8962374b8eb160b2cf29ad49c86a8cddfa8
Cris Lawrence <cris.lawrence@verizon.net>
2018-12-31 15:09:49 -0500

    Documentation

--- beers/constants.py
+++ beers/constants.py
@@ -7,5 +7,9 @@ Constants = namedtuple('Constants', ["AUDIT_FILENAME",
                                      'STDOUT_SUBDIRECTORY_NAME',
                                      'STDERR_SUBDIRECTORY_NAME',
                                      'DIRECTION_CONVENTION'])
+Constants.__doc__ = """
+Provides a list of constants for the BEERS ecosystem.  Constants for steps are not included here since those are
+subject to change by the user.
+"""
 
 CONSTANTS = Constants("audit.txt", 100, 'data', 'logs', 'stdout', 'stderr', [1, 2])
