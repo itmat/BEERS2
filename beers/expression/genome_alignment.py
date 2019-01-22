fb95c4d0d44c4ef1a8f4af214ecdfff6c8ebea9b
Cris Lawrence <cris.lawrence@verizon.net>
2019-01-22 17:15:57 -0500

    Somewhat better stub for alignment

--- beers/expression/genome_alignment.py
+++ beers/expression/genome_alignment.py
@@ -5,6 +5,17 @@ class GenomeAlignmentStep():
 
     name = "Genome Alignment Step"
 
+    sample_map = {
+        "1002": "Test_data.1002_baseline.sorted.bam",
+        "1015": "Test_data.1015_baseline.sorted.bam",
+        "ILB_9577": "ILB_9577.Aligned.sortedByCoord.out.bam",
+        "ILB_9578": "ILB_9578.Aligned.sortedByCoord.out.bam",
+        "ILB_9583": "ILB_9583.Aligned.sortedByCoord.out.bam",
+        "UNT_9576": "UNT_9576.Aligned.sortedByCoord.out.bam",
+        "UNT_9580": "UNT_9580.Aligned.sortedByCoord.out.bam",
+        "UNT_9584": "UNT_9584.Aligned.sortedByCoord.out.bam"
+    }
+
     def __init__(self, logfile, data_directory_path, parameters):
         self.data_directory_path = data_directory_path
 
@@ -12,16 +23,15 @@ class GenomeAlignmentStep():
         return True
 
     def execute(self, sample, reference_genome):
-        # TODO Stub!!  Hard-coded 2 alignment files to stub for STAR alignments.
+        # TODO Stub!!  Hard-coded alignment files to stub for STAR alignments.
         alignment_file_path = ''
         alignment_index_file_path = ''
         parent_dir = os.path.abspath(os.path.join(sample.input_file_paths[0], os.pardir))
-        if "1002" in sample.sample_name:
-            alignment_file_path = os.path.join(parent_dir,"Test_data.1002_baseline.sorted.bam")
-            alignment_index_file_path = os.path.join(parent_dir, "Test_data.1002_baseline.sorted.bai")
-        if "1015" in sample.sample_name:
-            alignment_file_path = os.path.join(parent_dir,"Test_data.1015_baseline.sorted.bam")
-            alignment_index_file_path = os.path.join(parent_dir, "Test_data.1015_baseline.sorted.bai")
+        for key, value in GenomeAlignmentStep.sample_map.items():
+            if key in sample.sample_name:
+                alignment_file_path = os.path.join(parent_dir, value)
+                alignment_index_file_path = os.path.join(parent_dir, value + ".bai")
+                break
         shutil.copyfile(alignment_file_path,
                         os.path.join(self.data_directory_path, f'sample{sample.sample_id}', 'genome_alignment.bam'))
         shutil.copyfile(alignment_index_file_path,
