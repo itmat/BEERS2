a3e5008ce4a970ec10e5b9d1c44b154bcbd030bc
brainfood <967448+brainfood@users.noreply.github.com>
2019-06-10 09:53:14 -0400

    Remove CAMPAREE directory (preparing for submodule).

--- CAMPAREE/camparee/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-The camparee package contains the camparee module along with all the modules that
-perform the RNA sample simulation.
-"""
--- CAMPAREE/setup.py
+++
@@ -1,5 +0,0 @@
-# Use pip install -e .
-
-from setuptools import setup, find_packages
-
-setup(name='CAMPAREE', version='0.1-alpha', packages=find_packages())
