810d415605946a8df8c23af708e5073a20616357
brainfood <967448+brainfood@users.noreply.github.com>
2019-06-10 09:55:07 -0400

    Remove BEERS_UTILS directory (preparing for submodule).

--- BEERS_UTILS/beers_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-The beers_utils package contains assorted modules requires across the BEERS suite
-of packages.
-"""
--- BEERS_UTILS/setup.py
+++
@@ -1,5 +0,0 @@
-# Use pip install -e .
-
-from setuptools import setup, find_packages
-
-setup(name='BEERS_UTILS', version='0.1-alpha', packages=find_packages())
