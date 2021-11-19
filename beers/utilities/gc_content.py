c065528426688813cae4bdc72759c5696bde31f0
tgbrooks <tgbrooks@gmail.com>
2021-11-19 13:06:08 -0500

    Add GC content plots to summary util

--- beers/utilities/gc_content.py
+++ beers/utilities/gc_content.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
 import numpy as np
 from beers_utils.molecule_packet import MoleculePacket
+from beers_utils import cigar
 
-def gc_content(molecule):
+def gc_content(molecule, aligned_only):
     seq = molecule.sequence
+    if aligned_only:
+        # Extract just the parts of seq that aligned to reference
+        i = 0
+        segments = []
+        for op, num in cigar.split_cigar(molecule.source_cigar):
+            if cigar.consumes[op]['query']:
+                if cigar.consumes[op]['ref']:
+                    segments.append(seq[i:i+num])
+                i += num
+        seq = ''.join(segments)
+
+    if len(seq) == 0:
+        return float("NaN")
     gc = (seq.count('C') + seq.count('G') + seq.count('N')/2) / len(seq)
     return gc
 
-def packet_gc_content(molecule_packet):
+def packet_gc_content(molecule_packet, aligned_only=False):
     '''
     Assess the GC content distribution of a molecule packet
 
     Returns gc content per every 2.5%, i.e. percent molecules in each GC content bin
     of 0-2.5%, 2.5-5%,  ... 97.5-100%
+
+    aligned_only: if True, then restrict to the bases of the molecule sequence that
+                    aligned to the reference sequence, according to the source_cigar
+                    So, for example, polyA tails and adapters won't be counted.
+                    Default is to count the entire molecule sequence
     '''
 
-    gcs = [gc_content(mol) for mol in molecule_packet.molecules]
+    gcs = [gc_content(mol, aligned_only) for mol in molecule_packet.molecules]
 
     bins = np.linspace(0, 1, 41)
     bins[-1] = 1.001 # make it right-inclusive for the last bin
--- beers/utilities/summarize_output.py
+++ beers/utilities/summarize_output.py
@@ -20,6 +20,7 @@ import pysam
 from beers_utils.read_fasta import read_fasta
 from beers_utils.molecule_packet import MoleculePacket
 from beers_utils.molecule import Molecule
+from beers.utilities.gc_content import packet_gc_content
 from beers.cluster_packet import ClusterPacket
 from beers_utils import cigar
 
@@ -70,6 +71,23 @@ def plot_pileups(pileups_dict, reference):
             ax.set_xlim(0, length)
         fig.savefig(pileup_dir / f"{chrom}.png", dpi=300)
 
+def plot_gcs(gcs_dict):
+    fig, ax = pylab.subplots(
+            figsize = (5, 5)
+    )
+
+    for name, gcs in gcs_dict.items():
+        bins = [bin for dist, bin in gcs]
+        dists = [dist for dist, bin in gcs]
+        dist = numpy.concatenate((numpy.nanmean(dists, axis=0), [0]))
+        ax.plot(bins[0], dist, label=name)
+    ax.set_ylabel("Percent Reads")
+    ax.set_xlabel("Percent GC")
+    ax.xaxis.set_major_formatter(matplotlib.ticker.PercentFormatter(1.0))
+    ax.yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter(1.0))
+    ax.legend()
+    return fig
+
 outdir = pathlib.Path(args.outdir)
 outdir.mkdir(exist_ok=True)
 
@@ -82,9 +100,13 @@ input_dir = pathlib.Path(config['library_prep_pipeline']['input']['directory_pat
 input_samples = input_dir.rglob("**/molecule_file.txt")
 
 input_pileups = make_pileups(reference_genome)
+input_gcs = []
+input_aligned_gcs = []
 for sample in input_samples:
     packet = MoleculePacket.from_CAMPAREE_molecule_file(sample, packet_id=0)
     pileup(packet.molecules, input_pileups)
+    input_gcs.append(packet_gc_content(packet))
+    input_aligned_gcs.append(packet_gc_content(packet, aligned_only=True))
 
 output_dir = pathlib.Path(config['controller']['output_directory_path'] + f"_run{args.run_id}")
 lib_prep_out = output_dir/"library_prep_pipeline/data/"
@@ -92,29 +114,38 @@ lib_prep_out = output_dir/"library_prep_pipeline/data/"
 lib_prep_molecule_packets = lib_prep_out.rglob("**/library_prep_pipeline_result_molecule_pkt*.txt")
 
 lib_prep_pileups = make_pileups(reference_genome)
+lib_prep_gcs = []
+lib_prep_aligned_gcs = []
 for packet in lib_prep_molecule_packets:
     packet = MoleculePacket.deserialize(packet)
     pileup(packet.molecules, lib_prep_pileups)
+    lib_prep_gcs.append(packet_gc_content(packet))
+    lib_prep_aligned_gcs.append(packet_gc_content(packet, aligned_only=True))
 
 seq_out = output_dir/"controller/data/"
 sam_files = seq_out.rglob("*.sam")
 seq_pileups = make_pileups(reference_genome)
+seq_gcs = []
+seq_aligned_gcs = []
 for sam_file in sam_files:
     molecules = []
+    #TODO: handle overlapping reads from the same fragment properly
     with pysam.AlignmentFile(str(sam_file), "r") as sam:
         for read in sam:
             strand = '-' if ((read.is_reverse and read.is_read1) or (not read.is_reverse and read.is_read2)) else '+'
             molecules.append(Molecule(
                 start = 1,
                 molecule_id = 0,
-                sequence = "",
+                sequence = read.query_sequence,
                 source_chrom = read.reference_name,
                 source_start = read.reference_start + 1,
                 source_cigar = read.cigarstring,
                 source_strand = strand,
             ))
     pileup(molecules, seq_pileups)
-
+    packet = MoleculePacket(0, None, molecules)
+    seq_gcs.append(packet_gc_content(packet))
+    seq_aligned_gcs.append(packet_gc_content(packet, aligned_only=True))
 
 plot_pileups(
     {
@@ -123,3 +154,23 @@ plot_pileups(
         "Seq": seq_pileups
     },
     reference=reference_genome)
+
+gc_dir = outdir/"gcs"
+gc_dir.mkdir(exist_ok=True)
+fig = plot_gcs(
+    {
+        "CAMPAREE": input_gcs,
+        "Lib Prep": lib_prep_gcs,
+        "Seq": seq_gcs
+    }
+)
+fig.savefig(gc_dir / "gc.png", dpi=300)
+
+fig = plot_gcs(
+    {
+        "CAMPAREE": input_aligned_gcs,
+        "Lib Prep": lib_prep_aligned_gcs,
+        "Seq": seq_aligned_gcs
+    }
+)
+fig.savefig(gc_dir / "gc.aligned_only.png", dpi=300)
