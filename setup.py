bb8b541f97d55bd3fff7fd56c8d08d469613c490
tgbrooks <tgbrooks@gmail.com>
2022-09-23 09:18:55 -0400

    Install cython extension in setup.py
    Otherwise not available unless doing a -e install

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 # Use pip install -e .
 
 from setuptools import setup, find_packages
+from Cython.Build import cythonize
 
 setup(
     name='beers',
     version='2.0',
     packages=find_packages(),
     scripts=["bin/run_beers.py"],
+    ext_modules = cythonize(["beers/sequence/sequence_by_synthesis_helper.pyx"]),
 )
