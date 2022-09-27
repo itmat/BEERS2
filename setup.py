d71d451326dca46fbbe7ce03074df62f8940aa10
tgbrooks <tgbrooks@gmail.com>
2022-09-27 11:50:09 -0400

    Move to src/ directory
    Plays better with setup.py installation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='beers',
     version='2.0',
     packages=find_packages(),
-    scripts=["bin/run_beers.py"],
-    ext_modules = cythonize(["beers/sequence/sequence_by_synthesis_helper.pyx"]),
+    scripts=["bin/run_beers"],
+    ext_modules = cythonize(["src/beers/sequence/sequence_by_synthesis_helper.pyx"]),
     include_dirs=[numpy.get_include()],
 )
