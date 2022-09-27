1ace61aa15b1797ec6001c4d398c96cdd659e3aa
tgbrooks <tgbrooks@gmail.com>
2022-09-27 11:49:01 -0400

    Add numpy headers

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name='beers',
@@ -9,4 +10,5 @@ setup(
     packages=find_packages(),
     scripts=["bin/run_beers.py"],
     ext_modules = cythonize(["beers/sequence/sequence_by_synthesis_helper.pyx"]),
+    include_dirs=[numpy.get_include()],
 )
