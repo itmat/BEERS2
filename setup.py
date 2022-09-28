a19cc9d2150e63c8d1463955f275d47f6de429c5
tgbrooks <tgbrooks@gmail.com>
2022-09-28 09:49:17 -0400

    Fix installations

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# Use pip install -e .
-
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy
@@ -7,8 +5,10 @@ import numpy
 setup(
     name='beers',
     version='2.0',
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     scripts=["bin/run_beers"],
-    ext_modules = cythonize(["src/beers/sequence/sequence_by_synthesis_helper.pyx"]),
+    ext_modules = cythonize(["src/beers/sequence/sequence_by_synthesis_helper.pyx"], language_level=3),
+    package_data = {"beers": ["Snakefile"]},
     include_dirs=[numpy.get_include()],
 )
